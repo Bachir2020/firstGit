2ec0d373346f8e19fcf4ede968bfe24f1f78816b
Bachir2020 <bachir.atmani.2011@gmail.com>
2020-06-02 20:35:49 +0200

    ceci est juste un test

--- openpyxlDemo.py
+++ openpyxlDemo.py
@@ -14,6 +14,7 @@ for i in range (1,sheet.max_row+1):
     if sheet.cell(row=i,column=1).value=="test2":
         for j in range(2,sheet.max_column+1):
 
+
             Dic1[sheet.cell(row=1,column=j).value] = sheet.cell(row=i,column=j).value
 
 print(Dic)
