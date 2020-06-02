4b84d0eaa66ff80a910feae4459b7e051487c334
Bachir2020 <bachir.atmani.2011@gmail.com>
2020-06-02 20:05:04 +0200

    this is commit from testeur2

--- openpyxlDemo.py
+++ openpyxlDemo.py
@@ -4,10 +4,17 @@ book = openpyxl.load_workbook("C:\\Bachir\\Formations\\Selenium_Python\\e2eFrame
 sheet = book.active #se pointer sur l'onglet actif
 
 Dic={}
+Dic1={}
 for i in range (1,sheet.max_row+1):
     if sheet.cell(row=i,column=1).value=="test33":
         for j in range(2,sheet.max_column+1):
 
             Dic[sheet.cell(row=1,column=j).value] = sheet.cell(row=i,column=j).value
 
+    if sheet.cell(row=i,column=1).value=="test2":
+        for j in range(2,sheet.max_column+1):
+
+            Dic1[sheet.cell(row=1,column=j).value] = sheet.cell(row=i,column=j).value
+
 print(Dic)
+print(Dic1)
